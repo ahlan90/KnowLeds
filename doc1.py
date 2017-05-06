1d17ade4a50f8db371d5d014ab8a2ade2ba398b6
Ahlan Guarnier <ahlan90@gmail.com>
2017-05-06 20:30:47 +0000

    Correção 2

--- doc1.py
+++ doc1.py
@@ -11,37 +11,29 @@ def get_taiga_status(request, nomeProjeto):
         pass
     
     if request.method == 'POST':
-        
         ## Captura o JSON enviado pelo Taiga
         r = json.load(request)
-        
-        ## Identifica a action enviada pelo Taiga, que pode ser create, change, delete ou teste
+        ## Identifica a action enviada pelo Taiga
         if r['action'] == 'create' :
-           
             # Se for SPRINT
             if r['type'] == 'milestone':
-                
                 ## Executa o parse de Projeto e Salva
                 projeto = projeto_save(r['data']['project'], projeto_knowleds)
-                
                 ## Salva o Sprint
                 sprint_save(r['data'], projeto)
 
 
 ## Converte os atributos do JSON para os atributos da classe e salva
 def projeto_save(jsonLoad, projetoKnowLeds):
-    
     # Pesquisa no banco se já existe o projeto salvo
     if Projeto.objects.filter(projeto_id=jsonLoad['id']):
         projeto = Projeto.objects.filter(projeto_id=jsonLoad['id'])[0]
     else:
         projeto = Projeto()
-    
+
     projeto.projeto_id = jsonLoad['id']
     projeto.permalink = jsonLoad['permalink']
     projeto.nome = jsonLoad['name']
     projeto.projeto_knowleds = projetoKnowLeds
-    
     projeto.save()
-    
     return projeto
